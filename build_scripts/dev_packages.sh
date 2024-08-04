f49d07ec48110f7c71c8a079f0005f965bcd265a
Halvor Kvamme <flkz@proton.me>
2024-08-04 15:12:50 +0200

    Use rust from fedora, use containers if wanting rustup

--- build_scripts/dev_packages.sh
+++ build_scripts/dev_packages.sh
@@ -19,16 +19,29 @@ IDE_PACKAGES=(
 curl -Lo /etc/yum.repos.d/_copr_gmaglione-podman-bootc-fedora-"${RELEASE}".repo https://copr.fedorainfracloud.org/coprs/gmaglione/podman-bootc/repo/fedora-"${RELEASE}"/gmaglione-podman-bootc-fedora-"${RELEASE}".repo
 # Images (Podman, Qemu, Bootc dev)
 IMG_PACKAGES=(
+    # Vm
     qemu
     qemu-img
+    virt-manager
 
     podman-bootc
 )
 
 # Rust
-COMMON_DEPENDENCIES=(
-    gcc
+RUST=(
+    rust
+    cargo
+    rust-analyzer
+    rustfmt
+    clippy
+    
+    # Dev deps
     wayland-devel
+    libxkbcommon-devel
+    libseat-devel
+    libgbm-devel
+    libinput-devel
+    systemd-devel
 )
 
-rpm-ostree install "${IDE_PACKAGES[@]}" "${IMG_PACKAGES[@]}"
+rpm-ostree install "${IDE_PACKAGES[@]}" "${IMG_PACKAGES[@]}" "${RUST[@]}"
